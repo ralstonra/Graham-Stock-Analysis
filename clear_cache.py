89c7f1359382f7fa7b6e9e199bbacb405a5bec62
BobbyR <ralstonra@yahoo.com>
2025-05-31 04:22:54 -0700

    Remove files not present in faad5d7, add.gitignore

--- clear_cache.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect(r"C:\Users\ralst\OneDrive\Public\stock_analysis\data\api_cache.db")
-cursor = conn.cursor()
-cursor.execute("DELETE FROM stocks")
-conn.commit()
-conn.close()
-print("Cleared the stocks table.")
--- test_score.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-from graham_data import calculate_graham_score_8, calculate_common_criteria, fetch_historical_data
-
-def test_graham_score():
-    score = calculate_graham_score
